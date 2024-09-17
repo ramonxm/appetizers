93ed859397f581b22ec5d0df3e0f31b51f0564c5
Ramon Xavier <ramon1234rxm@gmail.com>
2024-09-16 22:03:15 -0300

    first commit

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerTabView.swift
+//  Appetizers
+//
+//  Created by Ramon Xavier on 16/09/24.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            Text("Home")
+        }
+    }
+}
+
+#Preview {
+    AppetizerTabView()
+}
---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ramon Xavier on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
